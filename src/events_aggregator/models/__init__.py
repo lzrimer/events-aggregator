03c361a8653208324fbfc4cdbdc2eb09d9c195be
Liza Rimer <lzrimer@gmail.com>
2026-09-02 23:25:17 +0700

    fix SyncStatus export

--- src/events_aggregator/models/__init__.py
+++ src/events_aggregator/models/__init__.py
@@ -1,11 +1,12 @@
 from events_aggregator.models.event import Event
 from events_aggregator.models.place import Place
-from events_aggregator.models.sync import SyncMetadata
+from events_aggregator.models.sync import SyncMetadata, SyncStatus
 from events_aggregator.models.ticket import Ticket
 
 __all__ = [
     "Event",
     "Place",
     "SyncMetadata",
+    "SyncStatus",
     "Ticket",
 ]
